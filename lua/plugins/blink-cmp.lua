e0569843bcad178c7f50d0289b5ea0f62689d447
lcberg <50460080+lcberg@users.noreply.github.com>
2025-08-18 16:19:38 +0200

    autocomplete improvements
    add more colors and information for autocomplete popup (blink-cmp)

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -13,13 +13,13 @@ return {
 
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
-	opts = {
+	opts = function(_, opts)
 		-- 'default' for mappings similar to built-in completion
 		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
 		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
 		-- see the "default configuration" section below for full documentation on how to define
 		-- your own keymap.
-		keymap = {
+		opts.keymap = {
 			preset = "default",
 			-- ["<Tab>"] = { "select_and_accept", "fallback" },
 			["<Tab>"] = {
@@ -39,9 +39,9 @@ return {
 			["<C-h>"] = { "snippet_forward", "fallback" },
 			["<C-l>"] = { "snippet_backward", "fallback" },
 			["<C-s>"] = { "show_signature", "hide_signature", "fallback" },
-		},
+		}
 
-		appearance = {
+		opts.appearance = {
 			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
 			-- Useful for when your theme doesn't support blink.cmp
 			-- will be removed in a future release
@@ -49,11 +49,31 @@ return {
 			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = "mono",
-		},
+			kind_icons = {
+				Snippet = "",
+			},
+		}
+		opts.cmdline = {
+
+			completion = {
+				menu = {
+					auto_show = true,
+				},
+				ghost_text = {
+					enabled = false,
+				},
+				list = {
+					selection = {
+						preselect = false,
+						auto_insert = true,
+					},
+				},
+			},
+		}
 
 		-- default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, via `opts_extend`
-		sources = {
+		opts.sources = {
 			default = { "lsp", "buffer", "snippets", "lazydev", "path" },
 			providers = {
 				lazydev = {
@@ -62,10 +82,8 @@ return {
 					score_offset = 100,
 				},
 			},
-			-- optionally disable cmdline completions
-			-- cmdline = {},
-		},
-		signature = {
+		}
+		opts.signature = {
 			enabled = true,
 			trigger = {
 				enabled = true,
@@ -73,12 +91,45 @@ return {
 				show_on_trigger_character = true,
 				show_on_insert = true,
 			},
-		},
-		fuzzy = { implementation = "prefer_rust_with_warning" },
+		}
+		opts.fuzzy = { implementation = "prefer_rust_with_warning" }
+		opts.completion = {
+			menu = {
+				draw = {
+					columns = { { "kind_icon" }, { "label", gap = 1 }, { "source" } },
+					components = {
+						label = {
+							text = require("colorful-menu").blink_components_text,
+							highlight = require("colorful-menu").blink_components_highlight,
+						},
+						source = {
+							text = function(ctx)
+								local map = {
+									["lsp"] = "[]",
+									["path"] = "[󰉋]",
+									["snippets"] = "[]",
+								}
+
+								return map[ctx.item.source_id]
+							end,
+							highlight = "BlinkCmpDoc",
+						},
+					},
+				},
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 100,
+				update_delay_ms = 50,
+				window = {
+					max_width = math.min(80, vim.o.columns),
+				},
+			},
+		}
 
 		-- experimental signature help support
 		-- signature = { enabled = true }
-	},
+	end,
 	-- allows extending the providers array elsewhere in your config
 	-- without having to redefine it
 	opts_extend = { "sources.default" },
---
+++ lua/plugins/colorful-menus.lua
@@ -0,0 +1,5 @@
+return {
+	"xzbdmw/colorful-menu.nvim",
+	-- event = { "InsertEnter", "CmdlineEnter" },
+	opts = {},
+}
