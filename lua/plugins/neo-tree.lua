d1f4fc9a208dfea8cce058eba3b55397d5b5e199
lcberg <50460080+lcberg@users.noreply.github.com>
2026-02-06 11:36:08 +0100

    fix web devicons config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,17 +1,38 @@
-local function contains(t, value)
-	for _, v in ipairs(t) do
-		if v == value then
-			return true
-		end
-	end
-end
-
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		{
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			config = function()
+				require("nvim-web-devicons").setup({
+					override_by_filename = {
+						[".env.development"] = {
+							icon = "",
+							highlight = "NeoTreeDotfile",
+						},
+						[".env.example"] = {
+							icon = "",
+							highlight = "NeoTreeDotfile",
+						},
+						[".env.production"] = {
+							icon = "",
+							highlight = "NeoTreeDotfile",
+						},
+						[".env"] = {
+							icon = "",
+							highlight = "NeoTreeDotfile",
+						},
+						[".test"] = {
+							icon = "",
+							highlight = "NeoTreeDotfile",
+						},
+					},
+				})
+			end,
+		},
+		-- "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
@@ -32,26 +53,26 @@ return {
 					enabled = true,
 				},
 			},
-			default_component_configs = {
-				icon = {
-					provider = function(icon, node)
-						local name = node.name
-
-						local dotfiles = {
-							".env",
-							".env.example",
-							".env.development",
-							".env.production",
-							".env.test",
-						}
-
-						if contains(dotfiles, name) then
-							icon.text = ""
-							icon.highlight = "NeoTreeDotfile"
-						end
-					end,
-				},
-			},
+			-- default_component_configs = {
+			-- 	icon = {
+			-- 		provider = function(icon, node)
+			-- 			local name = node.name
+			--
+			-- 			local dotfiles = {
+			-- 				".env",
+			-- 				".env.example",
+			-- 				".env.development",
+			-- 				".env.production",
+			-- 				".env.test",
+			-- 			}
+			--
+			-- 			if contains(dotfiles, name) then
+			-- 				icon.text = ""
+			-- 				icon.highlight = "NeoTreeDotfile"
+			-- 			end
+			-- 		end,
+			-- 	},
+			-- },
 		})
 	end,
 }
